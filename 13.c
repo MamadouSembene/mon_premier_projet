ef683abe3fc7d9161d3f4154b59489b77096cc71
MamadouSembene <mouxammadou@gmail.com>
2020-11-09 15:59:43 +0000

    projet2

---
+++ 13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main ( int argc, char *argv[]){
+/*int main (void){ 
+double  prix = 123.25;
+printf("le nombre est %.4f\n", prix);*/
+signed char nom = 'a' ;
+printf(" sa nom :");
+scanf("%c",& nom);
+printf("donc sa tour moye %c", nom); 
+return 0;
+
+ /*float PI = 3.14;
+printf(" PI est %.2f",PI);
+PI = 554.5;
+printf(" PI est %.2f",PI);*/
+
+}
